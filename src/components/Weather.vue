d97bf8ea657e92e0ce752b8acb1d76a1bb83b30f
Lucien Lee <lkiral7903@gmail.com>
2017-07-24 15:04:36 +0800

    add root component

---
+++ src/components/Weather.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+
+</template>
+
+<script>
+export default {
+  name: 'weather'
+}
+</script>
+
+
+<style lang="sass" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Weather from '@/components/Weather'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Weather',
+      component: Weather
     }
   ]
 })
